b04a8c6277bb9bed89a604ff058e6301672926d8
AGUILAR VARGAS RICARDO ANTONIO <eb10141@titanic.ecci.ucr.ac.cr>
2016-11-23 07:34:54 +0000

    Poner ficha valida

--- Controlador.php
+++ Controlador.php
@@ -7,21 +7,24 @@
 			return "HOLAMUNDO";
 		}
 	}*/
-	
+
 	function imprimir(){
 			return "HOLAMUNDO";
 		}
+	// Verifica si en el tablero actual hay un ganador
 	function gano($matrix){;
 		$ha_ganado = false;
-		$ha_ganado = revisar($matrix); 
+		$ha_ganado = revisar($matrix);
 		return $ha_ganado;
 	}
-	
+
+	// Revisa todas las posibles opciones de ganar
 	function revisar ($matrix){
 			return (revisar_horizontal($matrix) || revisar_vertical($matrix) || revisar_diagonales($matrix));
 	}
-	
+
 	function revisar_horizontal($matrix){
+		// Verifica si hay algun ganador en las filas del tablero
 		$ha_ganado = false;
 		for($i=0 ; $i<3 ;++$i){
 			if( $matrix[$i][0] == $matrix[$i][1] && $matrix[$i][0] == $matrix[$i][2] && $matrix[$i][0] != -1 ){
@@ -30,7 +33,8 @@
 		}
 		return $ha_ganado;
 	}
-	
+
+	// Verifica si hay algun ganador en las columnas del tablero
 	function revisar_vertical($matrix){
 		$ha_ganado = false;
 		for($i=0 ; $i<3 ;++$i){
@@ -40,7 +44,8 @@
 		}
 		return $ha_ganado;
 	}
-	
+
+	// Verifica si hay algun ganador en las diagonales del tablero
 	function revisar_diagonales($matrix){
 		$ha_ganado = false;
 		if($matrix[0][0] == $matrix[1][1] && $matrix[0][0]==$matrix[2][2] && $matrix[0][0] != -1 ){
@@ -49,9 +54,9 @@
 		if($matrix[0][2] == $matrix[1][1] && $matrix[0][2]==$matrix[2][0] && $matrix[0][2] != -1 ){
 			$ha_ganado = true;
 		}
-			
+
 		return $ha_ganado;
 	}
-	
+
 	//$obj = new Controlador();
-?> 
+?>
--- servidor.php
+++ servidor.php
@@ -1,9 +1,40 @@
 <?php
 	include 'Controlador.php';
-	
-	$tablero [0] [0] = 0; $tablero [0] [1] = 1; $tablero [0] [2] = -1; 
-	$tablero [1] [0] = 1; $tablero [1] [1] = -1; $tablero [1] [2] = 1; 
-	$tablero [2] [0] = 0; $tablero [2] [1] = -1; $tablero [2] [2] = 0; 
+
+	$tablero [0] [0] = -1; $tablero [0] [1] = -1; $tablero [0] [2] = -1;
+	$tablero [1] [0] = -1; $tablero [1] [1] = -1; $tablero [1] [2] = -1;
+	$tablero [2] [0] = -1; $tablero [2] [1] = -1; $tablero [2] [2] = -1;
+
+	$turno_jugador = 0;
+
+	/*
+	Pone la ficha del jugador en turno en la posicion que se solicita
+	*/
+	function poner_ficha($fila,$columna){
+		if(esta_vacia($fila,$columna) && es_valida($fila,$columna)){
+			$tablero[$fila][$columna] = $turno_jugador;
+			if($turno_jugador == 0 ){
+				jugar_computadora();
+			}
+		}
+	}
+
+	//Verifica que una casilla del tablero esta vacia
+	function esta_vacia($fila, $columna){
+		return ($tablero[$fila][$columna] == -1);
+	}
+
+	// Verifica que las coordenadas sean validas en el tablero
+	function es_valida($fila, $columna){
+		return ($fila < 3 && $fila >= 0 && $columna < 3 && $columna >= 0);
+	}
+
+	// Hace la jugada "inteligente" del computador
+	function jugar_computadora(){
+
+	}
+
+
 	if(gano($tablero)){
 		echo "Gano";
 	}else{
