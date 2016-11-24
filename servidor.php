37e1ef99ec5eda6555355ea0162c8a9348757813
AGUILAR VARGAS RICARDO ANTONIO <eb10141@titanic.ecci.ucr.ac.cr>
2016-11-24 19:49:36 +0000

    Clase Servidor

--- servidor.php
+++ servidor.php
@@ -1,45 +1,59 @@
 <?php
 	include 'Controlador.php';
 
-	$tablero [0] [0] = -1; $tablero [0] [1] = -1; $tablero [0] [2] = -1;
-	$tablero [1] [0] = -1; $tablero [1] [1] = -1; $tablero [1] [2] = -1;
-	$tablero [2] [0] = -1; $tablero [2] [1] = -1; $tablero [2] [2] = -1;
+	/**
+	 *
+	 */
+	class Servidor{
+		public $tablero;
+		private $turno_jugador = 0;
 
-	$turno_jugador = 0;
+		function __construct(){
+			$tablero [0] [0] = -1; $tablero [0] [1] = -1; $tablero [0] [2] = -1;
+			$tablero [1] [0] = -1; $tablero [1] [1] = -1; $tablero [1] [2] = -1;
+			$tablero [2] [0] = -1; $tablero [2] [1] = -1; $tablero [2] [2] = -1;
+		}
 
-	/*
-	Pone la ficha del jugador en turno en la posicion que se solicita
-	*/
-	function poner_ficha($fila,$columna){
-		if(esta_vacia($fila,$columna) && es_valida($fila,$columna)){
-			$tablero[$fila][$columna] = $turno_jugador;
-			if($turno_jugador == 0 ){
-			//	jugar_computadora();
+		/*
+		Pone la ficha del jugador en turno en la posicion que se solicita
+		*/
+		public function poner_ficha($fila,$columna){
+			if($this->esta_vacia($fila,$columna) && $this->es_valida($fila,$columna)){
+				$tablero[$fila][$columna] = $turno_jugador;
+				if($turno_jugador == 0 ){
+				//	jugar_computadora();
+				}
 			}
 		}
-	}
 
-	//Verifica que una casilla del tablero esta vacia
-	function esta_vacia($fila, $columna){
-		return ($tablero[$fila][$columna] == -1);
-	}
+		//Verifica que una casilla del tablero esta vacia
+		private function esta_vacia($fila, $columna){
 
-	// Verifica que las coordenadas sean validas en el tablero
-	function es_valida($fila, $columna){
-		return ($fila < 3 && $fila >= 0 && $columna < 3 && $columna >= 0);
-	}
+			return ($this->$tablero[$fila][$columna] == -1);
+		}
 
-	// Hace la jugada "inteligente" del computador
-	function jugar_computadora(){
+		// Verifica que las coordenadas sean validas en el tablero
+		private function es_valida($fila, $columna){
+			return ($fila < 3 && $fila >= 0 && $columna < 3 && $columna >= 0);
+		}
+
+		// Hace la jugada "inteligente" del computador
+		private function jugar_computadora(){
+
+		}
+
+		private function  imprimir(){
+			if(gano($tablero)){
+				echo "HAY UN GANADOR";
+			}else{
+				echo "NO HAY UN GANADOR";
+			}
+		}
+
+
 
 	}
-
-	poner_ficha(0,1);
-	poner_ficha(0,0);
-	poner_ficha(0,2);
-	if(gano($tablero)){
-		echo "Gano";
-	}else{
-		echo "Perdio";
-	}
+	$mServidor = new Servidor();
+	$mServidor->poner_ficha(0,0);
+	$mServidor->poner_ficha(0,1);
 ?>
