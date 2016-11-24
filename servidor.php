7e7bb8ff175189eeecf298b5b62f5e1729624abc
AGUILAR VARGAS RICARDO ANTONIO <eb10141@titanic.ecci.ucr.ac.cr>
2016-11-24 19:58:59 +0000

    Clase v1

--- servidor.php
+++ servidor.php
@@ -9,9 +9,9 @@
 		private $turno_jugador = 0;
 
 		function __construct(){
-			$tablero [0] [0] = -1; $tablero [0] [1] = -1; $tablero [0] [2] = -1;
-			$tablero [1] [0] = -1; $tablero [1] [1] = -1; $tablero [1] [2] = -1;
-			$tablero [2] [0] = -1; $tablero [2] [1] = -1; $tablero [2] [2] = -1;
+			$this->tablero [0] [0] = -1; $this->tablero [0] [1] = -1; $this->tablero [0] [2] = -1;
+			$this->tablero [1] [0] = -1; $this->tablero [1] [1] = -1; $this->tablero [1] [2] = -1;
+			$this->tablero [2] [0] = -1; $this->tablero [2] [1] = -1; $this->tablero [2] [2] = -1;
 		}
 
 		/*
@@ -19,8 +19,8 @@
 		*/
 		public function poner_ficha($fila,$columna){
 			if($this->esta_vacia($fila,$columna) && $this->es_valida($fila,$columna)){
-				$tablero[$fila][$columna] = $turno_jugador;
-				if($turno_jugador == 0 ){
+				$tablero[$fila][$columna] = $this->turno_jugador;
+				if($this->turno_jugador == 0 ){
 				//	jugar_computadora();
 				}
 			}
@@ -29,7 +29,7 @@
 		//Verifica que una casilla del tablero esta vacia
 		private function esta_vacia($fila, $columna){
 
-			return ($this->$tablero[$fila][$columna] == -1);
+			return ($this->tablero[$fila][$columna] == -1);
 		}
 
 		// Verifica que las coordenadas sean validas en el tablero
@@ -42,11 +42,11 @@
 
 		}
 
-		private function  imprimir(){
-			if(gano($tablero)){
-				echo "HAY UN GANADOR";
+		public function  imprimir(){
+			if(gano($this->tablero)){
+				print "HAY UN GANADOR\n"	;
 			}else{
-				echo "NO HAY UN GANADOR";
+				print "NO HAY UN GANADOR \n";
 			}
 		}
 
@@ -56,4 +56,7 @@
 	$mServidor = new Servidor();
 	$mServidor->poner_ficha(0,0);
 	$mServidor->poner_ficha(0,1);
+	$mServidor->imprimir();
+	$mServidor->poner_ficha(0,2);
+	$mServidor->imprimir();
 ?>
