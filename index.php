3acb0746368eb7dc27249b4342824b7585bb58ee
raguilar0 <eb10141@titanic.ecci.ucr.ac.cr>
2016-11-28 18:02:44 +0000

    Quitando HEAD

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 require_once 'Servidor.php';
 
-<<<<<<< HEAD
 ini_set('soap.wsdl_cache_enabled', '0');
 ini_set('soap.wsdl_cache_ttl', '0');
 
@@ -23,25 +22,3 @@ else{
 	$servidorSoap->handle();
 }
 ?>
-=======
-  if (isset($_GET['wsdl'])) {
-		header('Content-Type: application/soap+xml; charset=utf-8');
-		echo file_get_contents('gato.wsdl');
-	}
-	else {
-		session_start();
-		$servidorSoap = new SoapServer('http://titanic.ecci.ucr.ac.cr:80/~eb10141/tic_tac_toe/?wsdl');
-
-		//Para evitar la excepción por defecto de SOAP PHP cuando no existe HTTP_RAW_POST_DATA,
-		//se regresa explícitamente el siguiente fallo cuando no hay solicitud (v.b. desde un navegador)
-		if(!@$HTTP_RAW_POST_DATA){
-			$servidorSoap->fault('SOAP-ENV:Client', 'Invalid Request');
-			exit;
-	}
-
-	$servidorSoap->setClass('Servidor');
-	$servidorSoap->setPersistence(SOAP_PERSISTENCE_SESSION);
-	$servidorSoap->handle();
-
-?>
->>>>>>> 196bd43932b2073b9e4665f9dcb4a5597d33a7fd
